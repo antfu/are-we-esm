1ca4a966c05091b80dc11332a277421a7f031188
pacexy <pacexy@gmail.com>
2025-01-15 21:38:02 +0800

    fix: handle package.json with UTF-8 BOM

--- src/run.ts
+++ src/run.ts
@@ -2,12 +2,14 @@ import type { ExtendedPackageNode, ResolvedPackageNode } from './types'
 import fs from 'node:fs/promises'
 import { join } from 'node:path'
 import { analyzePackageJson } from './analyze'
+import { stripBomTag } from './utils'
 
 export async function analyzePackage(pkg: ExtendedPackageNode): Promise<ResolvedPackageNode> {
   const _pkg = pkg as unknown as ResolvedPackageNode
   if (_pkg.type)
     return _pkg
-  const json = JSON.parse(await fs.readFile(join(pkg.path, 'package.json'), 'utf-8'))
+  const content = await fs.readFile(join(pkg.path, 'package.json'), 'utf-8')
+  const json = JSON.parse(stripBomTag(content))
   _pkg.type = analyzePackageJson(json)
   return _pkg
 }
